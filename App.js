10bf20a5fc725c9dee53f2652cbb06b5d799d26f
Manasa Pachava <manasapachava99@gmail.com>
2024-03-15 06:21:55 +0530

    secod episode

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 const parent = React.createElement("div",
 {id:"parent"},
 [React.createElement("div",
 {id:"child"},
-[React.createElement("h1",{},"hI am h1"),React.createElement("h2",{},"hI am h2")]),
+[React.createElement("h1",{},"hI am h1 tage"),React.createElement("h2",{},"hI am h2")]),
 React.createElement("div",
 {id:"child2"},
 [React.createElement("h1",{},"Manasa"),React.createElement("h2",{},"Manoj")])]);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Namasthy react
+
+#parcel
+ -Dev Build
+ -local server
+ -HMR = Hot Module Replacement
+ -file watching algorithm -c++
+ -caching -faster builds
+ -Image optimization
+ -minification of files bundling 
+ -compress the files
+ -consistent hashing
+ -code splitting
+ -diffrerential bundling -- support older browsers
+ -Tree shaking-- remove unused code
+ -diffrent dev and prod builds
+ 
